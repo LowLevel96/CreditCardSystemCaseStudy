08a72e108353800f3f5da5bff0a72cf44a5c2394
Dusan Stojnic <dusan.stojnic.mes@gmail.com>
2018-05-13 10:30:20 -0400

    Changing java project name and mapping documents

--- MappingDocuments/MappingSQLCrediCardTable.sql
+++ MappingDocuments/MappingSQLCrediCardTable.sql
@@ -1,6 +1,4 @@
-DROP VIEW IF EXISTS CDW_SAPP_F_CREDIT_CARD;
-
-CREATE VIEW `CDW_SAPP_F_CREDIT_CARD` AS SELECT t.id, cc.credit_card_no, CONCAT(cc.day, cc.month, cc.year) as TIMEID, c.ssn, b.branch_code, t.transaction_type, t.transaction_value
+SELECT t.id, cc.credit_card_no, CONCAT(cc.day, cc.month, cc.year) as TIMEID, c.ssn, b.branch_code, t.transaction_type, t.transaction_value
 FROM CreditCard cc JOIN Customer c ON (cc.customer=c.id)
 JOIN Branch b ON (b.id=cc.branch_code)
 JOIN Transactions t ON (cc.id=t.credit_card_no);
--- MappingDocuments/MappingSQLCustomerTable.sql
+++ MappingDocuments/MappingSQLCustomerTable.sql
@@ -1,7 +1,4 @@
-DROP VIEW IF EXISTS CDW_SAPP_D_CUSTOMER;
-
-
-CREATE VIEW `CDW_SAPP_D_CUSTOMER` AS SELECT c.ssn as CUST_SSN, c.first_name as CUST_F_NAME, c.middle_name as CUST_M_NAME, c.last_name as CUST_L_NAME,
+SELECT c.ssn as CUST_SSN, c.first_name as CUST_F_NAME, c.middle_name as CUST_M_NAME, c.last_name as CUST_L_NAME,
 											cc.credit_card_no as CUST_CC_NO, CONCAT(CONCAT(c.apt_no, ' '), c.street_name) as CUST_STREET,
                                             c.city as CUST_CITY, s.name as CUST_STATE, Countries.name as CUST_COUNTRY, c.zip as CUST_ZIP, c.phone as CUST_PHONE,
                                             c.email as CUST_EMAIL, c.LAST_UPDATED
